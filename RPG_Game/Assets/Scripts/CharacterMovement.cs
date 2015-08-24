393d4e95341ec19fe0ffab0397d61c99c3f1bc71
sasuke010101 <sasuke010101@gmail.com>
2015-08-24 16:15:37 +0800

    import character01 and make the movement script

---
+++ RPG_Game/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMovement : MonoBehaviour 
+{
+	private Rigidbody2D playerRigidBody2D;
+
+	private bool facingRight;
+
+	public float speed = 4.0f;
+
+	void Awake()
+	{
+		playerRigidBody2D = (Rigidbody2D)GetComponent(typeof(Rigidbody2D));
+	}
+
+	void Update()
+	{
+		float movePlayerVector = Input.GetAxis("Horizontal");
+
+		playerRigidBody2D.velocity = new Vector2(movePlayerVector * speed, playerRigidBody2D.velocity.y);
+
+		if(movePlayerVector > 0 && !facingRight)
+		{
+			Flip();
+		}else if( movePlayerVector < 0 && facingRight)
+		{
+			Flip();
+		}
+	}
+
+	void Flip()
+	{
+		facingRight = !facingRight;
+
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+
+}
---
+++ RPG_Game/Assets/Scripts/Entity.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Entity : ScriptableObject 
+{
+	public string Name;
+	public int Age;
+	string Faction;
+	public string Occupation;
+	public int Level = 1;
+	public int Health = 2;
+	public int Strength = 1;
+	public int Magic = 0;
+	public int Defense = 0;
+	public int Speed = 1;
+	public int Damage = 1;
+	public int Armor = 0;
+	public int NoOfAttacks = 1;
+	public string Weapon;
+	public Vector2 Position;
+
+	public void TakeDamage(int Amount) { Health -= (Amount - Armor); }
+	public void Attack(Entity Entity) { Entity.TakeDamage(Strength); }
+}
