d12134036c4c863b6e48069564656b1595e47b46
sasuke010101 <sasuke010101@gmail.com>
2015-08-25 12:02:48 +0800

    add followCamera like mario camera type and left right boder add to the world enter the left or right collision will show a box ask leave or stay

---
+++ RPG_Game/Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCamera : MonoBehaviour {
+
+	public float xMargin = 1.5f;
+	public float yMargin = 1.5f;
+
+	public float xSmooth = 1.5f;
+	public float ySmooth = 1.5f;
+
+	private Vector2 maxXAndY;
+	private Vector2 minXAndY;
+
+	public Transform player;
+
+	void Awake()
+	{
+		var backgroundBounds = GameObject.Find("background").renderer.bounds;
+		var camTopLeft = camera.ViewportToWorldPoint(new Vector3(0, 0, 0));
+		var camBottomRight = camera.ViewportToWorldPoint(new Vector3(1, 1, 0));
+
+		minXAndY.x = backgroundBounds.min.x - camTopLeft.x;
+		maxXAndY.x = backgroundBounds.max.x - camBottomRight.x;
+
+		player = GameObject.Find("Player").transform;
+
+		if(player == null)
+		{
+			Debug.LogError("Player object not found");
+		}
+	}
+
+	bool CheckXMargin()
+	{
+		return Mathf.Abs (transform.position.x - player.position.x) > xMargin;
+	}
+
+	bool CheckYMargin()
+	{
+		return Mathf.Abs(transform.position.y - player.position.y) > yMargin;
+	}
+
+	void FixedUpdate()
+	{
+		float targetX = transform.position.x;
+		float targetY = transform.position.y;
+
+		if(CheckXMargin())
+			targetX = Mathf.Lerp(transform.position.x, player.position.x, xSmooth * Time.fixedDeltaTime);
+		if(CheckYMargin())
+			targetY = Mathf.Lerp(transform.position.y, player.position.y, ySmooth * Time.fixedDeltaTime);
+
+		targetX = Mathf.Clamp(targetX, minXAndY.x, maxXAndY.x);
+		targetY = Mathf.Clamp(targetY, minXAndY.y, maxXAndY.y);
+
+		transform.position = new Vector3(targetX, targetY, transform.position.z);
+	}
+}
---
+++ RPG_Game/Assets/Scripts/NavigationPrompt.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NavigationPrompt : MonoBehaviour {
+
+	bool showDialog;
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		showDialog = true;
+	}
+
+	void OnCollisionExit2D(Collision2D col)
+	{
+		showDialog = false;
+	}
+
+	void OnGUI()
+	{
+		if(showDialog)
+		{
+			GUI.BeginGroup(new Rect(Screen.width / 2 - 150, 50, 300, 250));
+			GUI.Box(new Rect(0, 0, 300, 250), "");
+
+			GUI.Label(new Rect(15, 10, 300, 68), "Do you want to travel to " + this.tag + " ?");
+			if(GUI.Button(new Rect(55, 100, 180, 40), "Travel"))
+			{
+				showDialog =false;
+
+			}
+
+			if(GUI.Button(new Rect(55, 150, 180, 40),"Stay"))
+			{
+				showDialog = false;
+			}
+			
+			GUI.EndGroup();
+		}
+	}
+}
